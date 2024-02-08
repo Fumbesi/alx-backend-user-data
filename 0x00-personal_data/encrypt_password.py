ea15799f9f801f94a3d9314531e6c75cc108b8ec
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-08 21:19:08 +0200

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,7 +18,21 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed_password
 
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate a password against its hashed version.
+
+    Args:
+        hashed_password (bytes): The salted, hashed password.
+        password (str): The plain text password to be validated.
+
+    Returns:
+        bool: True if the password is valid, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
 if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
-    print(hash_password(password))
-    print(hash_password(password))
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
