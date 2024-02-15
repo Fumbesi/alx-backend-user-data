f709556de359b09e249883251c3c8737771fa006
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-02-15 07:04:56 -0800

    the environment variable AUTH_TYPE, If AUTH_TYPE is equal to session_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,22 +6,26 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
+
+# Determine the authentication mechanism based on AUTH_TYPE environment variable
 AUTH_TYPE = getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+else:
+    auth = None
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -29,20 +33,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.before_request
 def before_request() -> str:
@@ -65,8 +66,8 @@ def before_request() -> str:
     if auth.current_user(request) is None:
         abort(403)
 
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
